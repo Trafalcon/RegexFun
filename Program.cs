3798ffcdef8ac7082f263d382be717245e12db3e
Aaron Fuller <afuller6@atu.edu>
2019-07-17 00:11:04 -0500

    Created project environment.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegexFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
